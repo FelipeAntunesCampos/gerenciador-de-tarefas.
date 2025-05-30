221de972e78d220656fb0e958c9820bfc61636aa
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-05-30 08:43:47 -0300

    Adiciona novas tarefas

---
+++ tarefas.js
@@ -0,0 +1,9 @@
+/* No arquivo tarefas.js, crie um array chamado tarefas com pelo menos 10 tarefas fictícias, representadas por strings. Exemplo:
+*/
+
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+// Etapa 1: Adicionando Novas Tarefas
+tarefas.push('Jogar bola', 'Beber água') // adicionando um novo estilo
+console.log(tarefas)
+
